db5f4869723ec735a1e142b5942e34d1911e7b0f
JackDanielsClassic <burya3369@gmail.com>
2025-01-15 23:44:11 +0200

    day step 3: changed from 'for' to 'for of' and naming also

--- Tasks/day.js
+++ Tasks/day.js
@@ -4,11 +4,10 @@
 
 const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const parseDay = (day) => {
-  let i;
-  for (i = 0; i < weekDays.length; i++) {
-    if (day.startsWith(weekDays[i].toLowerCase())) {
-      return i + 1;
+const parseDay = (inputDay) => {
+  for (const day of weekDays) {
+    if (inputDay.startsWith(day.toLowerCase())) {
+      return weekDays.indexOf(day)+1;
     }
   }
   return -1;
