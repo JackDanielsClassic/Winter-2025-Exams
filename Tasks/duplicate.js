efcda544edda12a2ab0595cfadec74b587387e94
JackDanielsClassic <burya3369@gmail.com>
2025-01-15 23:16:12 +0200

    duplicate step 2: remove unnecessary loop and add array.fill

--- Tasks/duplicate.js
+++ Tasks/duplicate.js
@@ -5,11 +5,7 @@
 const duplicate = (value, N) => {
   if (N <= 0) return [];
   else {
-    let res = [];
-    for (let i = 0; i < N; i++) {
-      res[i] = value;
-    }
-    return res;
+    return Array(N).fill(value);
   }
 };
 
