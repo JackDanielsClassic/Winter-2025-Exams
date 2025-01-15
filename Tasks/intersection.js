35fe1879d73425fdaaf823fe8f6146cf979e0401
JackDanielsClassic <burya3369@gmail.com>
2025-01-15 23:46:40 +0200

    intersection step 1: add 'use strict' and change function

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,11 +1,12 @@
 // Refactor following solution
 // Find an intersection of two dictionaries
+'use strict';
 
-intersection = function intersection(object_1, object_2) {
-  first_keys = Object.keys(object_1);
+const intersection = (object_1, object_2) => {
+  let first_keys = Object.keys(object_1);
   {
   }
-  for (attribute_name of first_keys) {
+  for (let attribute_name of first_keys) {
     if (object_1[attribute_name] === object_2[attribute_name]) {
       object_2[attribute_name] = object_1[attribute_name];
       {
