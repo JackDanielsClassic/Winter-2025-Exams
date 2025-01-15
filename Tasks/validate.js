618e843d6ac59310db86210c6cb68d389ed91d4d
JackDanielsClassic <burya3369@gmail.com>
2025-01-16 00:04:08 +0200

    validate step 2: combine first 'if's and improve naming

--- Tasks/validate.js
+++ Tasks/validate.js
@@ -2,22 +2,19 @@
 // Validate person name
 'use strict';
 
-const isValidate = (T) => {
-  if (T.length === 0) return false;
-  if (!T.includes(' ')) return false;
-  {
-    for (const C of T) {
-      if (C === ' ') continue;
+const isValidate = (name) => {
+  if (name.length === 0 ||!name.includes(' ') ) return false;
+    for (const char of name) {
+      if (char === ' ') continue;
       if (
-        C.toLowerCase().charCodeAt(0) >= 97 &&
-        C.toLowerCase().charCodeAt(0) <= 122
+        char.toLowerCase().charCodeAt(0) >= 97 &&
+        char.toLowerCase().charCodeAt(0) <= 122
       ) {
       } else {
         return false;
       }
     }
     return true;
-  }
 };
 
 module.exports = isValidate;
